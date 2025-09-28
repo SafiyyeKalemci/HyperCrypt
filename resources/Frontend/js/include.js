f6b7d9680dffd073c7c71d79da7fa3cdfcabb9b1
SafiyyeKalemci <152517140+SafiyyeKalemci@users.noreply.github.com>
2025-09-28 22:00:53 +0300

    Layout added

--- resources/Frontend/js/include.js
+++ resources/Frontend/js/include.js
@@ -1,18 +1,11 @@
-// include.js
 document.addEventListener("DOMContentLoaded", () => {
-  // Mevcut sayfanın URL'sini al
-  const currentPath = window.location.pathname; // Örn: /resources/Frontend/Anasayfa/anasayfa.html
+  const basePath = "../"; // Anasayfa -> Frontend
 
-  // Base path: header/footer klasörünün kökü
-  const basePath = "/resources/Frontend/";
-
-  // Header yükle
   fetch(basePath + "header/header.html")
     .then(res => res.text())
     .then(data => document.getElementById("header").innerHTML = data)
     .catch(err => console.error("Header yüklenemedi:", err));
 
-  // Footer yükle
   fetch(basePath + "footer/footer.html")
     .then(res => res.text())
     .then(data => document.getElementById("footer").innerHTML = data)
